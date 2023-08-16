0bc2342e8e0b2c4ca10a0eb34c95c88a9befecf7
Sagnik <sagnikma@gmail.com>
2023-08-16 21:26:23 +0530

    add env variable for setting database environment

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,5 +6,6 @@ module.exports = {
     PORT: process.env.PORT,
     SALT_ROUNDS: +process.env.SALT_ROUNDS,
     JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRY: process.env.JWT_EXPIRY
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    NODE_ENV: process.env.NODE_ENV
 }
