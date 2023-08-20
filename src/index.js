bcfefa3cb79fbc19dfec65139c4910ac0bbb3b0e
Sagnik <sagnikma@gmail.com>
2023-08-20 17:21:04 +0530

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) =>{
+    res.send('<h3>API Documentation:</h3><a href="https://github.com/sagnikman/todolist-express#api-documentation">Link</a>');
+});
+
 app.use('/api', apiRoutes);
 
 PORT = ServerConfig.PORT || 3000;
